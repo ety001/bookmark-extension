890d5afa904b8559661d32f800c5267651db7ad9
ETY001 <ety001@domyself.me>
2016-02-15 12:54:40 +0800

    v2.2.2

--- js/show.js
+++ js/show.js
@@ -1,12 +1,11 @@
 $(function(){
-  $('#openpreview').hide();
-  $('#closepreivew').hide();
   var tab_id;
-  /*if(window.localStorage.preview_switch=='on'){
+  var url;
+  if(window.localStorage.preview_switch=='on'){
     $('#openpreview').hide();
   } else {
     $('#closepreivew').hide();
-  }*/
+  }
 
   var lang_req = [
     "appname",
@@ -18,7 +17,8 @@ $(function(){
     "block",
     "blocklist",
     "blockmsg",
-    "nouse"
+    "nouse",
+    "chat"
   ];
 
   var port = chrome.runtime.connect({name: "bookmark_manager_ety001"});
@@ -30,19 +30,35 @@ $(function(){
     port.postMessage({ctype:"remove_bookmark",cdata:tab_id});
   });
 
+  $('#openpreview').click(function(){
+    window.localStorage.preview_switch='on';
+    $(this).hide();
+    $('iframe').attr('src', url);
+    $('#closepreivew').show();
+  });
+
+  $('#closepreivew').click(function(){
+    window.localStorage.preview_switch='off';
+    $(this).hide();
+    $('iframe').attr('src', '');
+    $('#openpreview').show();
+  });
+
   port.postMessage({ctype:"getbookmark",cdata:false});
   port.postMessage({ctype:"lang", cdata:lang_req});
   port.onMessage.addListener(function(msg) {
-    console.log(msg);
+    //console.log(msg);
     var ctype = msg.ctype;
     var cdata = msg.cdata;
     switch (ctype) {
       case 'getbookmark':
         var title = cdata[0].title;
-        var url = cdata[0].url;
+        url = cdata[0].url;
         tab_id = cdata[0].id;
-        $('#bookmark_title').html(title);
-        $('iframe').attr('src', url);
+        $('#bookmark_title').html(title+" | "+url);
+        if(window.localStorage.preview_switch=='on'){
+          $('iframe').attr('src', url);
+        }
         $('#openinnewtab').attr('href', url);
         break;
       case 'lang':
@@ -55,6 +71,7 @@ $(function(){
         $('#donate').html(cdata.donate);
         $('#blockmsg').html(cdata.blockmsg);
         $('#nouse').html(cdata.nouse);
+        $('#chat').html(cdata.chat);
         break;
       case 'block':
         if(cdata){
