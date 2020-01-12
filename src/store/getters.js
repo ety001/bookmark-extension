f3b0608985ae492c90dd2245e92bada45fec449c
ETY001 <work@akawa.ink>
2020-01-12 16:04:46 +0800

    change store structure

--- src/store/getters.js
+++ src/store/getters.js
@@ -1,3 +1,8 @@
-export const status = state => state.status;
-export const random = state => state.random;
+export const frequencyCounter = state => state.frequencyCounter;
+
 export const waitingBookmarks = state => state.waitingBookmarks;
+export const blockedBookmarks = state => state.blockedBookmarks;
+
+export const notifyLocationList = state => state.notifyLocationList;
+
+export const config = state => state.config;
--- src/store/index.js
+++ src/store/index.js
@@ -1,5 +1,6 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
+import VuexPersistence from 'vuex-persist';
 
 import * as getters from './getters';
 import mutations from './mutations';
@@ -7,13 +8,25 @@ import * as actions from './actions';
 
 Vue.use(Vuex);
 
+const vuexLocal = new VuexPersistence({
+  storage: window.localStorage,
+});
+
 export default new Vuex.Store({
   state: {
-    random: true, // 随机展示
-    status: true, // 是否开启扩展
+    config: {
+      status: true, // 是否开启扩展
+      random: true, // 随机展示
+      frequency: 5, // mini 模式展示频度
+      currentNotifyLocation: 'top-right', // 当前 Mini 提醒框位置
+    },
+    frequencyCounter: 0, // mini模式展示频度计数器
     waitingBookmarks: [], // 所有待提醒书签
+    blockedBookmarks: [], // 屏蔽掉的书签
+    notifyLocationList: ['top-right', 'top-left', 'bottom-right', 'bottom-left'],
   },
   getters,
   mutations,
   actions,
+  plugins: [vuexLocal.plugin],
 });
